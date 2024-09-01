1d472517657c06628bcbcf391f3734e922a3e30f
Chibuike Favour <iamkorra03@gmail.com>
2024-09-02 00:23:57 +0100

    Python classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A Square class that will be used to instantiate an object"""
+
+
+class Square:
+
+    """An ampty class Square that defines a square.
+    It currently has no attributes nor properties.
+    """
+    pass
